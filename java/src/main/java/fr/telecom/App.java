4599c8657cf9b4e083349d9c2bb4c893f3bc4593
glegoux <gilles.legoux@gmail.com>
2016-09-20 23:06:28 +0200

    Fix package name without dash

--- java/src/main/java/fr/telecom/App.java
+++ java/src/main/java/fr/telecom/App.java
@@ -1,4 +1,4 @@
-package fr.telecom-paristech;
+package fr.telecom;
 
 /**
  * Hello world!
--- java/src/test/java/fr/telecom/AppTest.java
+++ java/src/test/java/fr/telecom/AppTest.java
@@ -1,4 +1,4 @@
-package fr.telecom-paristech;
+package fr.telecom;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
